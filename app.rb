09eca4e058f0dfe39090c67834f3a09dba0be6c6
jonathan senior <jonosenior@gmail.com>
2018-01-24 17:06:24 +0100

    Add fully working app without pretty css

--- app.rb
+++ app.rb
@@ -21,16 +21,15 @@ end
 
 get '/play' do
   get_all_session_data
-  redirect to('/win') if success?
-  redirect to('/lose') if failure?
+  redirect to('/win') if win?
+  redirect to('/lose') if lose?
+  session[:warning_message] = nil
   erb :play
 end
 
 post "/play" do
   get_all_session_data
   evaluate_guess
-  session[:guesses_remaining] -= 1
-  # work out if guess matches solution
   redirect "/play"
 end
 
@@ -41,7 +40,6 @@ end
 get '/lose' do
   erb :lose
 end
-
 
 def reset_variables
   solution = random_word.split("")
@@ -57,14 +55,18 @@ def get_all_session_data
   @incorrect_guesses = session[:incorrect_guesses]
   @solution = session[:solution]
   @guesses_remaining = session[:guesses_remaining]
-  @guess = params["guess"] if params["guess"]
+  @guess = params["guess"].downcase if params["guess"]
+  @warning_message = session[:warning_message]
 end
 
 def evaluate_guess
   if @solution.include?(@guess)
     session[:gapped_solution] = update_gapped_solution
+  elsif session[:incorrect_guesses].include?(@guess)
+    session[:warning_message] = "You've already guessed #{@guess}"
   else
     session[:incorrect_guesses] << @guess
+    session[:guesses_remaining] -= 1
   end
 end
 
@@ -74,53 +76,11 @@ def update_gapped_solution
   @gapped_solution
 end
 
-def success?
-  false
-  # @solution == @gapped_solution
+def win?
+  @solution == @gapped_solution
   # !@gapped_solution.include?("__")
 end
 
-def failure?
+def lose?
   @guesses_remaining < 1
 end
-
-
-
-
-
-#####
-# @good_guesses = []
-# @secret_word.length.times do
-#   @good_guesses << "_"
-# end
-# @bad_guesses = []
-# end
-#
-# def evaluate_guess(guess)
-#   if @secret_word.include?(guess)
-#     correct_letters = @secret_word.each_index.select { |index| @secret_word[index] == guess }
-#     correct_letters.each { |e| @good_guesses[e] = guess  }
-#   else
-#     @bad_guesses << guess
-#   end
-# end
-#
-#
-#
-# #### Jono's gapped solution
-#
-#
-# def return_indices_of_guess
-#   indices_of_matches = []
-#   @solution.split("").each_with_index do |letter, index|
-#     if letter == @guess then indices_of_matches.push(index) end
-#   end
-#   indices_of_matches
-# end
-#
-#
-# def update_gapped_solution(indices_of_matches, guess)
-#   indices_of_matches.each do |index_of_match|
-#     @gapped_solution[index_of_match] = guess
-#   end
-# end
